a1135348894647dca1ce153ab9e68cb1521a08f1
ReiMoon72 <rhelpalma6@gmail.com>
2026-02-15 18:25:26 +0800

    Updated navbar styling and links

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -3,8 +3,13 @@
 </script>
 
 <template>
-
- <h1>Hello World!</h1>
+    <h1>Contact Us</h1>
+    <p>
+        For inquiries, please contact us at:
+        <br />
+        Phone: (123) 456-7890
+        <br />
+        </p>
 
 </template>
 
